cf29ba6311e240a3d8085b32c262a3864807c196
rollip <86199091+rollip@users.noreply.github.com>
2024-03-13 19:59:14 +0300

    major changes in seminar mage template added font awesome

--- dentima_class/main/static/js/burger.js
+++ dentima_class/main/static/js/burger.js
@@ -32,3 +32,12 @@ function closeOnClick() {
 }
 
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});

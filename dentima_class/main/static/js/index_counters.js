07b1bfd230df5fbc023a74c1affa5cdc564edc7c
rollip <rollip@yandex.ru>
2024-03-19 17:06:44 +0300

    server commit

--- dentima_class/main/static/js/index_counters.js
+++ dentima_class/main/static/js/index_counters.js
@@ -1,7 +1,20 @@
-let el = document.getElementById('counter')
 
-window.onload = function()
-{ document.getElementById('counter_seminars').style.setProperty('--num', 50)
-  document.getElementById('counter_students').style.setProperty('--num', 700)
+$(document).ready(function() {
 
- }
+        $('.counter').each(function () {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+ 
+});  
+
+
+
+

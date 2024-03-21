2ad02336180e22e4c667d0f168a425bbbbd7a650
rollip <86199091+rollip@users.noreply.github.com>
2024-03-22 00:07:49 +0300

    // white noise

--- dentima_class/dentima_class/wsgi.py
+++ dentima_class/dentima_class/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
+from my_project import MyWSGIApp
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dentima_class.settings')
 
-application = get_wsgi_application()
+application = MyWSGIApp()
+application = WhiteNoise(application, root= BASE_DIR / 'static')

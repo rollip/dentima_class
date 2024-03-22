4c987dc1cf9bfced8cb08dc5b7854bbc7d75e3c3
rollip <86199091+rollip@users.noreply.github.com>
2024-03-22 15:27:09 +0300

    ssl

--- dentima_class/dentima_class/settings.py
+++ dentima_class/dentima_class/settings.py
@@ -136,7 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = 'rollip@yandex.ru'
 EMAIL_HOST_PASSWORD = 'ijdlizkiqlccehkf'

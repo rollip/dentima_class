e3381f00b94dc3e9c5f1a42e60df4d0954dc09ca
rollip <86199091+rollip@users.noreply.github.com>
2024-03-22 15:22:01 +0300

    // email port

--- dentima_class/dentima_class/settings.py
+++ dentima_class/dentima_class/settings.py
@@ -135,7 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'rollip@yandex.ru'

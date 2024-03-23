68dc3b77cb8b8b1c38025f105103eb769404381c
rollip <86199091+rollip@users.noreply.github.com>
2024-03-23 13:21:26 +0300

    review models.py added default images deleted useless images

--- dentima_class/main/models.py
+++ dentima_class/main/models.py
@@ -7,12 +7,12 @@ from django.dispatch.dispatcher import receiver
 
 
 class Lector(models.Model):
-    name = models.CharField(max_length=200,unique=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
-    image = models.ImageField(upload_to='lectors/images',blank=True,null=True)
-    specialization = models.CharField(max_length=100, default='стоматолог')
-    short_desc = models.TextField(max_length=2000)
-    long_desc = models.TextField(max_length=10000)
+    name = models.CharField(max_length=200,unique=True, verbose_name='Имя')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name=' Часть ссылки (автозаполняется)')
+    image = models.ImageField(upload_to='lector/images',default='lector/images/empty_lector.jpg', blank=True,null=True, verbose_name='Фотография', help_text='соотношение сторон - квадрат')
+    specialization = models.CharField(max_length=100, default='стоматолог', verbose_name='Специализация')
+    short_desc = models.TextField(max_length=1000, verbose_name='Короткое описание', help_text='<1000 символов')
+    long_desc = models.TextField(max_length=10000, verbose_name='Полное описание  (заголовок в формате <p class="h2 my-3"> День 1 </p>) ', help_text='<10000 символов')
 
 
     class Meta:
@@ -30,19 +30,17 @@ class Seminar(models.Model):
         if self.start_date > self.end_date:
             raise ValidationError('Дата окончания не может быть раньше даты начала')
 
-    name = models.CharField(max_length=200,unique=True)
-    slug = models.SlugField(max_length=250, unique=True, blank=True, null=True)
-    image_1 = models.ImageField(upload_to='seminar/images',blank=True,null=True)
-    image_2 = models.ImageField(upload_to='seminar/images',blank=True,null=True)
-    background_image = models.ImageField(upload_to='seminar/background',default='seminar/background/background7.jpg',blank=True,null=True)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    lector = models.ForeignKey(to=Lector, on_delete=models.PROTECT, related_name='lector')
-    lector_2 = models.ForeignKey(to=Lector, on_delete=models.PROTECT, related_name='lector_2', blank=True, null=True, default=None)
-    address = models.CharField(max_length=100, unique=False)
-    type = models.CharField(max_length=50, default='лекция')
-    food = models.CharField(max_length=50,  default='кофе-брейк')
-    description = models.TextField(max_length=10000)
+    name = models.CharField(max_length=200,unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=250, unique=True, blank=True, null=True, verbose_name='Часть ссылки (автозаполняется)')
+    seminar_image = models.ImageField(upload_to='seminar/image',default='seminar/images/empty_seminar.jpg',blank=True,null=True, verbose_name='Картинка' )
+    start_date = models.DateField(verbose_name='Начало')
+    end_date = models.DateField(verbose_name='Окончание')
+    lector = models.ForeignKey(to=Lector, on_delete=models.PROTECT, related_name='lector', verbose_name='Лектор 1')
+    lector_2 = models.ForeignKey(to=Lector, on_delete=models.PROTECT, related_name='lector_2', blank=True, null=True, default=None, verbose_name='Лектор 2')
+    address = models.CharField(max_length=100, unique=False, verbose_name='Адрес')
+    type = models.CharField(max_length=50, default='лекция' , verbose_name='лекция/практика')
+    food = models.CharField(max_length=50,  default='кофе-брейк', verbose_name='обед/кофе-брей')
+    description = models.TextField(max_length=10000, verbose_name='Описание (заголовок в формате <p class="h2 my-3"> День 1 </p>) ')
     pricing = models.TextField(max_length=3000)
 
     class Meta:
@@ -52,5 +50,3 @@ class Seminar(models.Model):
 
     def __str__(self):
         return self.name
-
-
